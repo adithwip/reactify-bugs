5106275720b4506315145794efc5011405412396
Adith <adithwidyapradipta@gmail.com>
2017-08-21 17:43:38 +0700

    Mulai mecah component dan berhasil muncul

---
+++ bugtracker/src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Title extends React.Component {
+	render() {
+		return <h1 className="title is-1">Bug Tracker <small>by HACKTIV8</small></h1>
+	}
+}
+
+export default Title;
--- bugtracker/src/index.js
+++ bugtracker/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bulma/css/bulma.css'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
