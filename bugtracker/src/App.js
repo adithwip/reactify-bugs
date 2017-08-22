a2b03584e8e34c39a43f5befa5f5c40a55d058d0
Adith <adithwidyapradipta@gmail.com>
2017-08-22 07:31:12 +0700

    Function udah berfungsi, layout masih sedikit kurang rapih nggak mirip aslinya. Tapi overall done

--- bugtracker/src/App.js
+++ bugtracker/src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import Title from './components/Title'
 import Form from './components/Form'
+import Footer from './components/Footer'
+import ListBugs from './components/ListBugs'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="Container">
+			<div className="App">
+				<div className="container">
 					<Title></Title>
 					<Form></Form>
 					<hr />
-      </div>
+					<ListBugs></ListBugs>
+				</div>
+				<Footer></Footer>
+			</div>
     );
   }
 }
--- bugtracker/src/index.js
+++ bugtracker/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bulma/css/bulma.css'
+// import './main.js'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
